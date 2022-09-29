781e44a85d340d870bc964dde7890073a9250c25
anas <anasben3issa@gmail.com>
2022-09-29 11:03:38 +0100

    cleaned code

--- server.js
+++ server.js
@@ -1,46 +1,44 @@
-'use strict';
+"use strict";
 
-const Hapi = require('@hapi/hapi');
+const Hapi = require("@hapi/hapi");
 
 const init = async () => {
-    
-        const server = Hapi.Server({
-            port: 3000,
-            host: 'localhost'
-        });
-    
-        server.route({
-            method: 'GET',
-            path: '/',
-            handler: (request, h) => {
-    
-                return '<h1>Hello World!</h1>';
-            }
-        });
+  const server = Hapi.Server({
+    port: 3000,
+    host: "localhost",
+  });
 
-        server.route({
-            method: 'GET',
-            path: '/users',
-            handler: (request, h) => {
-                return h.redirect('/');
-            }
-        });
+  server.route([
+    {
+      method: "GET",
+      path: "/",
+      handler: (request, h) => {
+        return "<h1>Hello World!</h1>";
+      },
+    },
+    {
+      method: "GET",
+      path: "/users",
+      handler: (request, h) => {
+        return h.redirect("/");
+      },
+    },
+    {
+      method: "GET",
+      path: "/{any*}",
+      handler: (request, h) => {
+        return "<h1>404 Not Found</h1>";
+      },
+    },
+  ]);
 
-        server.route({
-            method: 'GET',
-            path: '/{any*}',
-            handler: (request, h) => {
-                return '<h1>404 Not Found</h1>';
-            }
-        });
-    
-        await server.start();
-        console.log('Server running on %s', server.info.uri);
+  await server.start();
+  console.log("Server running on %s", server.info.uri);
 };
 
-process.on('unhandledRejection', (err) => {
-    console.log(err);
-    process.exit(1);
+process.on("unhandledRejection", (err) => {
+  console.log(err);
+  process.exit(1);
 });
 
 init();
